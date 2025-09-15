39e78af8d93ae64e30671e9d16d5eb9b676fe11d
Emily Yang <u0835353@utah.edu>
2025-09-15 12:46:24 -0600

    Copied over the same file structure from previous labs

---
+++ include/func.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <pico/stdlib.h>
+
+#define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
+#define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
+
+#define SIDE_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
+#define SIDE_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
+
+void side_thread(void *params);
+void main_thread(void *params);
---
+++ test/test.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <pico/stdlib.h>
+#include "pico/cyw43_arch.h"
+#include <stdint.h>
+#include <unity.h>
+#include "unity_config.h"
+#include "func.h"
+
+void setUp(void) 
+{        
+    printf("Start test\n");
+}
+
+void tearDown(void) 
+{
+    printf("Finished test\n");
+}
+
+void test_variable_assignment()
+{
+    int x = 1;
+    TEST_ASSERT_TRUE_MESSAGE(x == 1,"Variable assignment failed.");
+}
+
+void test_multiplication(void)
+{
+    int x = 30;
+    int y = 6;
+    int z = x / y;
+    TEST_ASSERT_TRUE_MESSAGE(z == 5, "Multiplication of two integers returned incorrect value.");
+}
+
+int main (void)
+{
+    stdio_init_all();
+    hard_assert(cyw43_arch_init() == PICO_OK);
+    
+    sleep_ms(10000); // Give time for TTY to attach.
+    UNITY_BEGIN();
+    RUN_TEST(test_variable_assignment);
+    RUN_TEST(test_multiplication);
+    sleep_ms(5000);
+    UNITY_END();
+
+    while(1) { sleep_ms(5000); }
+
+    return UNITY_END();
+}
